2da2cb161033e5eb86cb3d6ec64721a6c0c1e10a
Spencer <cotton2_24@yahoo.com>
2021-04-10 16:02:07 -0600

    testing loading... pt 3

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -20,7 +20,7 @@ class PlayerCard extends React.Component {
             setTimeout(function(){ 
             console.log(loading);
             loading.style.display = "none";
-            }, );
+            }, 10);
 
             // console.log(loading);
             // loading.style.display = "none";
