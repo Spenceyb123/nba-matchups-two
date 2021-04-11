274ac3ad2e191164064260119d671587f14ee943
Spencer <cotton2_24@yahoo.com>
2021-04-11 11:27:23 -0600

    Updates

---
+++ precache-manifest.5b3f6f7ac85d173121483a7e71bf2a5d.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2398b5fb50b769393252fa57ab5f576",
+    "url": "/nba-matchups-two/index.html"
+  },
+  {
+    "revision": "6446bf32bb14bbe49cc8",
+    "url": "/nba-matchups-two/static/css/main.4f9f1eeb.chunk.css"
+  },
+  {
+    "revision": "cb33757574915c19c340",
+    "url": "/nba-matchups-two/static/js/2.5125fe05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nba-matchups-two/static/js/2.5125fe05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6446bf32bb14bbe49cc8",
+    "url": "/nba-matchups-two/static/js/main.4db9de30.chunk.js"
+  },
+  {
+    "revision": "05993bddeaebf94e8351",
+    "url": "/nba-matchups-two/static/js/runtime-main.62c2e714.js"
+  },
+  {
+    "revision": "4a37c1bd8d8c1f1107b8bb86f788bbcf",
+    "url": "/nba-matchups-two/static/media/allen01.4a37c1bd.jpg"
+  },
+  {
+    "revision": "919f53cd86e620be00ac7ce3fe9c4f52",
+    "url": "/nba-matchups-two/static/media/antetokoumpo19.919f53cd.jpg"
+  },
+  {
+    "revision": "b32f15277be11e88cc82453e03b25420",
+    "url": "/nba-matchups-two/static/media/arenas06.b32f1527.jpg"
+  },
+  {
+    "revision": "2e76e66ec1de142c9ef996a6c552f653",
+    "url": "/nba-matchups-two/static/media/background-confetti.2e76e66e.png"
+  },
+  {
+    "revision": "fc5eea4a9f8b2f34a67aed16acb715c4",
+    "url": "/nba-matchups-two/static/media/barkley93.fc5eea4a.jpg"
+  },
+  {
+    "revision": "008361cee12cc3b432ab1d56775c1a1a",
+    "url": "/nba-matchups-two/static/media/basketball-button-start-game.008361ce.png"
+  },
+  {
+    "revision": "5c177cdbc9f7daec06a8f936a3fb0e6c",
+    "url": "/nba-matchups-two/static/media/basketball-button.5c177cdb.png"
+  },
+  {
+    "revision": "e5c922bb2516246e347377bcbcafeabc",
+    "url": "/nba-matchups-two/static/media/bball_court.e5c922bb.jpg"
+  },
+  {
+    "revision": "28d7cf98b2fb036651ed8329daf875f5",
+    "url": "/nba-matchups-two/static/media/billups06.28d7cf98.jpg"
+  },
+  {
+    "revision": "027da693ee9c9eec7104a3721484e8f0",
+    "url": "/nba-matchups-two/static/media/bird86.027da693.jpg"
+  },
+  {
+    "revision": "36c9018ad7942f998ac90bf76ba70f50",
+    "url": "/nba-matchups-two/static/media/brand06.36c9018a.jpg"
+  },
+  {
+    "revision": "a62382feb671efc3402f0485cf9f706e",
+    "url": "/nba-matchups-two/static/media/bryant03.a62382fe.jpg"
+  },
+  {
+    "revision": "afc462f194312e32b3bf22bb53d7922d",
+    "url": "/nba-matchups-two/static/media/bryant08.afc462f1.jpg"
+  },
+  {
+    "revision": "3376f3f5bb1387c68f37a8a406cb2478",
+    "url": "/nba-matchups-two/static/media/butler17.3376f3f5.jpg"
+  },
+  {
+    "revision": "069676ecb22f61878c091be7fb35a70b",
+    "url": "/nba-matchups-two/static/media/crying-jordan.069676ec.png"
+  },
+  {
+    "revision": "e2571afcbcf7b312e89541dac64c2a3b",
+    "url": "/nba-matchups-two/static/media/curry16.e2571afc.jpg"
+  },
+  {
+    "revision": "0400c3a2f859931fc933fcbcacded754",
+    "url": "/nba-matchups-two/static/media/davis15.0400c3a2.jpg"
+  },
+  {
+    "revision": "95568742b3e48374b964f07af4edab36",
+    "url": "/nba-matchups-two/static/media/duncan02.95568742.jpg"
+  },
+  {
+    "revision": "2bf11969938e2218b28ecefe957ebcad",
+    "url": "/nba-matchups-two/static/media/durant14.2bf11969.jpg"
+  },
+  {
+    "revision": "d3ffafede08fbdc80368f83caafe43e5",
+    "url": "/nba-matchups-two/static/media/garnett04.d3ffafed.jpg"
+  },
+  {
+    "revision": "34ef13385429b0804977793bcba4b4a4",
+    "url": "/nba-matchups-two/static/media/gasol11.34ef1338.jpg"
+  },
+  {
+    "revision": "875f353e4a3c237ba222ae39c473c4bf",
+    "url": "/nba-matchups-two/static/media/hardaway96.875f353e.jpg"
+  },
+  {
+    "revision": "15a2eba4b76eaf9ba471cc3cc54e118e",
+    "url": "/nba-matchups-two/static/media/harden15.15a2eba4.jpg"
+  },
+  {
+    "revision": "dcfd717a0928817f94152bb58ea4563c",
+    "url": "/nba-matchups-two/static/media/hill97.dcfd717a.jpg"
+  },
+  {
+    "revision": "c3f56353ac4407612a7d0931a7f06ea8",
+    "url": "/nba-matchups-two/static/media/howard11.c3f56353.jpg"
+  },
+  {
+    "revision": "56e1df4adf803864be31d0804d0978b2",
+    "url": "/nba-matchups-two/static/media/james06.56e1df4a.jpg"
+  },
+  {
+    "revision": "3c97080fae5630668be44101a20a6b81",
+    "url": "/nba-matchups-two/static/media/james13.3c97080f.jpg"
+  },
+  {
+    "revision": "12fd9ff902ffe060601d4f06653233a4",
+    "url": "/nba-matchups-two/static/media/james18.12fd9ff9.jpg"
+  },
+  {
+    "revision": "b2036b5597e9a4e5248cd4d25dc82559",
+    "url": "/nba-matchups-two/static/media/johnson91.b2036b55.jpg"
+  },
+  {
+    "revision": "6055610f8968af690df45dae8eeadcaf",
+    "url": "/nba-matchups-two/static/media/jordan91.6055610f.jpg"
+  },
+  {
+    "revision": "3c94367420ab895daa76c361cfcb177c",
+    "url": "/nba-matchups-two/static/media/jordan96.3c943674.jpg"
+  },
+  {
+    "revision": "9d69b68fe194bfef3250374b92b40eaf",
+    "url": "/nba-matchups-two/static/media/jordan98.9d69b68f.jpg"
+  },
+  {
+    "revision": "d364871dfae9c6ff2b0ba2543df6833f",
+    "url": "/nba-matchups-two/static/media/leonard16.d364871d.jpg"
+  },
+  {
+    "revision": "5d32de7b68fda5122ca64feafcae8ffe",
+    "url": "/nba-matchups-two/static/media/love14.5d32de7b.jpg"
+  },
+  {
+    "revision": "ed2e16bd495bb31d32e73f7a7a941de8",
+    "url": "/nba-matchups-two/static/media/malone91.ed2e16bd.jpg"
+  },
+  {
+    "revision": "bb7ab96f7faef7544de9a0ca2ce8bb82",
+    "url": "/nba-matchups-two/static/media/malone97.bb7ab96f.jpg"
+  },
+  {
+    "revision": "261fb24e4a7085fcae22e3d2b4f7456e",
+    "url": "/nba-matchups-two/static/media/marion06.261fb24e.jpg"
+  },
+  {
+    "revision": "6f02b5fe81a6a7ead2e0d24140dd1ce1",
+    "url": "/nba-matchups-two/static/media/mcgrady03.6f02b5fe.jpg"
+  },
+  {
+    "revision": "df727281653b16e5eb218b7782c87ee9",
+    "url": "/nba-matchups-two/static/media/mtg-card.df727281.png"
+  },
+  {
+    "revision": "76e42ddd9e9cca7c9b004f9d57537352",
+    "url": "/nba-matchups-two/static/media/nba-scoreboard.76e42ddd.png"
+  },
+  {
+    "revision": "96d3529c4b8792ba1840f3deac24b3cd",
+    "url": "/nba-matchups-two/static/media/nba-trophy.96d3529c.png"
+  },
+  {
+    "revision": "02cc124ac672c6cbd53eb6206dc9e21c",
+    "url": "/nba-matchups-two/static/media/nowitzki06.02cc124a.jpg"
+  },
+  {
+    "revision": "927fc9534ea7454576a391610c14aa53",
+    "url": "/nba-matchups-two/static/media/olajuwon93.927fc953.jpg"
+  },
+  {
+    "revision": "84d4a978db92af7576fbfaf8b880bb11",
+    "url": "/nba-matchups-two/static/media/paul08.84d4a978.jpg"
+  },
+  {
+    "revision": "d7bc23dacbb9a514692b889609a1dad1",
+    "url": "/nba-matchups-two/static/media/paul15.d7bc23da.jpg"
+  },
+  {
+    "revision": "321dbdebc42b874f5e447a35d6a401d1",
+    "url": "/nba-matchups-two/static/media/payton00.321dbdeb.jpg"
+  },
+  {
+    "revision": "b293ee8d1184b0cdd99a2b0f06066c3b",
+    "url": "/nba-matchups-two/static/media/players.b293ee8d.csv"
+  },
+  {
+    "revision": "b1d686c5e2554d804654ac1dc90a6d8d",
+    "url": "/nba-matchups-two/static/media/ref-angry-rotate.b1d686c5.jpg"
+  },
+  {
+    "revision": "cd6c68c1e12ac580002712755f8d17de",
+    "url": "/nba-matchups-two/static/media/robinson96.cd6c68c1.jpg"
+  },
+  {
+    "revision": "46feef019cc55af41d1936c0bcf20963",
+    "url": "/nba-matchups-two/static/media/roy09.46feef01.jpg"
+  },
+  {
+    "revision": "d66b24444322a5a5ce1bffbd0900ea7e",
+    "url": "/nba-matchups-two/static/media/shaq00.d66b2444.jpg"
+  },
+  {
+    "revision": "a39386fea55fc0ea3e6275f2c162c3b4",
+    "url": "/nba-matchups-two/static/media/shaq94.a39386fe.jpg"
+  },
+  {
+    "revision": "e918674b37bad0ad71cf7451d328f7af",
+    "url": "/nba-matchups-two/static/media/stockton91.e918674b.jpg"
+  },
+  {
+    "revision": "ef8666a4232cf432dd41c6612f86445c",
+    "url": "/nba-matchups-two/static/media/stockton97.ef8666a4.jpg"
+  },
+  {
+    "revision": "8943c0f4640b4df2299f8a4bf984746f",
+    "url": "/nba-matchups-two/static/media/stoudemire05.8943c0f4.jpg"
+  },
+  {
+    "revision": "a049cccd6bf61b02d7ea7a4371b44be3",
+    "url": "/nba-matchups-two/static/media/towns18.a049cccd.jpg"
+  },
+  {
+    "revision": "b7ec70302761d5817e8e43c4d395dc1e",
+    "url": "/nba-matchups-two/static/media/wade09.b7ec7030.jpg"
+  },
+  {
+    "revision": "8b302eb8143825e31a7dc6032d6e377b",
+    "url": "/nba-matchups-two/static/media/westbrook16.8b302eb8.jpg"
+  }
+]);
